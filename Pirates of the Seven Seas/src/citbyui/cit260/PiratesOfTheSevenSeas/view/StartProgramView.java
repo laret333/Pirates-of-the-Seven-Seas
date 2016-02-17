8b288fa11cde393964076244d8dd667394e6f042
Tanner <Tanner@10.18.149.37>
2016-02-17 12:30:35 -0700

    2/17/16 Started the view layer and included the description.

---
+++ Pirates of the Seven Seas/src/citbyui/cit260/PiratesOfTheSevenSeas/view/StartProgramView.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package citbyui.cit260.PiratesOfTheSevenSeas.view;
+
+/**
+ *
+ * @author Tanner
+ */
+public class StartProgramView {
+    
+   
+    public StartProgramView() {
+    
+        this.displayBanner();
+        
+    }
+
+    private void displayBanner() {
+        
+        System.out.println(
+            "\n************************************************************"
+            "\n* Welcome Captain Steel Eagle!                             *"
+            "\n*                                                          *"
+            "\n* You are in the sea of Spokania, which has the            *"
+            "\n* port to the first sea that you are to conquer.           *"
+            "\n* Your mission is to conquer all seven seas by plundering  *"
+            "\n* and attacking all enemies that dare stand in your way!   *"
+            "\n*                                                          *"
+            "\n* Because of the battles that you and your pirates will    *"
+            "\n* fight, you must wisely use the gold that you steal       *"
+            "\n* to buy supplies and food, recruit and train new priates, *"
+            "\n* and repair and upgrade your ship. You must keep your     *"
+            "\n* supplies and equipment in order to defeat your enemies   *"
+            "\n* and ultimately conquer the blasted Captain Cannon in     *"
+            "\n* the Great North Sea!                                     *"
+            
+            )
+        
+        
+    }
+}
